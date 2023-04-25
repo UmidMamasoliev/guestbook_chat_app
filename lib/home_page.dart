3e9ee1fbf85309c69aa1b7c4e486fe94e8a30d7e
UmidMamasoliev <umidjon.mamasoliev@itechart-group.com>
2023-04-25 21:29:27 +0500

    guestbook chat app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 import 'app_state.dart';
@@ -14,6 +15,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dateFormat = DateFormat.MMMMd().format(DateTime.now());
     return Scaffold(
       appBar: AppBar(
         title: const Text('Firebase Meetup'),
@@ -22,8 +24,8 @@ class HomePage extends StatelessWidget {
         children: <Widget>[
           Image.asset('assets/codelab.png'),
           const SizedBox(height: 8),
-          const IconAndDetail(Icons.calendar_today, 'October 30'),
-          const IconAndDetail(Icons.location_city, 'San Francisco'),
+          IconAndDetail(Icons.calendar_today, dateFormat),
+          const IconAndDetail(Icons.location_city, 'Tashkent'),
           // Add from here
           Consumer<ApplicationState>(
             builder: (context, appState, _) => AuthFunc(
@@ -42,7 +44,8 @@ class HomePage extends StatelessWidget {
           ),
           const Header("What we'll be doing"),
           const Paragraph(
-            'Join us for a day full of Firebase Workshops and Pizza!',
+            'Join us for a day full of Firebase Workshops'
+            ' and Pizza at T.Shevchenko, 21A!',
           ),
           Consumer<ApplicationState>(
             builder: (context, appState, _) => Column(
